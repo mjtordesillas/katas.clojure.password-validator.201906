3a3d31e90d7993743baafa9314506c62065fa6ee
mjtordesillas <you@example.com>
2019-06-12 09:02:39 +0200

    refactor: extracted functions for symmetry and legibility, made them private

--- src/password_validator/core.clj
+++ src/password_validator/core.clj
@@ -2,19 +2,25 @@
   (:gen-class)
   (:require [clojure.string :refer :all]))
 
+(defn- long-enough? [password]
+  (>= (count password) 8))
+
+(defn- contains-underscore? [password]
+  (includes? password "_"))
+
 (defn- contains-number? [password]
   (some? (re-find #"[0-9]+" password)))
 
-(defn contains-lowercase? [password]
+(defn- contains-lowercase? [password]
   (some? (re-find #"[a-z]+" password)))
 
-(defn contains-uppercase? [password]
+(defn- contains-uppercase? [password]
   (some? (re-find #"[A-Z]+" password)))
 
 (defn valid? [password]
   (and
-    (>= (count password) 8)
-    (includes? password "_")
+    (long-enough? password)
+    (contains-underscore? password)
     (contains-number? password)
     (contains-lowercase? password)
     (contains-uppercase? password)))
