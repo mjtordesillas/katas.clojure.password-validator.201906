4e09e5b6bcace8fb854db6d0fc9701c14694cd55
mjtordesillas <you@example.com>
2019-06-12 08:25:04 +0200

    valid? returns true for valid passwords

--- src/password_validator/core.clj
+++ src/password_validator/core.clj
@@ -1,7 +1,5 @@
 (ns password-validator.core
   (:gen-class))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(defn valid? [password]
+  true)
--- test/password_validator/core_test.clj
+++ test/password_validator/core_test.clj
@@ -2,6 +2,6 @@
   (:require [clojure.test :refer :all]
             [password-validator.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest password-validator
+  (testing "Valid password"
+    (is (= true (valid? "Aa8_nnnn")))))
